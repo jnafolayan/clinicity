f4f629fc2dfe861d69e0ee6a65861749065173eb
John Afolayan <jnafolayan@gmail.com>
2020-06-08 20:51:16 +0100

    add search field

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(4, 3)
+  }
+}));
+
+const useSearchFieldStyles = makeStyles((theme) => ({
+  root: {
+  }
+}));
+
+type UserCoords = { latitude: number; longitude: number };
+
+const getUserCoords: () => Promise<UserCoords> = () => 
+  new Promise((resolve, reject) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(pos => resolve(pos.coords));
+    } else {
+      reject(new Error("Geolocation is not supported by this browser."));
+    }
+  });
+
+const App: React.FC = () => {
+  const searchFieldClasses = useSearchFieldStyles();
+  const classes = useStyles();
+  const [searchRadius, setSearchRadius] = useState<number>(0);
+
+  useEffect(() => {
+    (async () => {
+      const coords: UserCoords = await getUserCoords();
+      console.log(coords);
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <form action="" method="post">
+        <Grid container>
+          <Grid item md={4}></Grid>
+          <Grid item md={4}>
+            <TextField 
+              id="search" 
+              label="Search radius" 
+              type="number"
+              InputProps={{
+                classes: searchFieldClasses,
+                startAdornment: <InputAdornment position="start">km</InputAdornment>
+              }}
+              variant="outlined" 
+              fullWidth 
+            />
+          </Grid>
+        </Grid>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
